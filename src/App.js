956b61545383be8c9dd26e240bccf76c5eba651b
GrayHead <ss.zhuravlov@gmail.com>
2022-01-29 10:40:24 +0200

    test pull request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
 function App() {
-  return (
-    <>
-    </>
-  );
+    return (
+        <>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, ad adipisci aliquid atque cupiditate,
+            deserunt eos eum fugit illo ipsam, iste nisi praesentium repellendus sapiente tempore veniam voluptate!
+            Dolores, hic!
+        </>
+    );
 }
 
 export default App;
